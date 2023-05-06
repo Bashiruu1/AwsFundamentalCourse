1a2b75a972181d304f68193aaee545a359d50e1f
Usman <bashiruu1@gmail.com>
2023-05-06 03:49:37 -0400

    actually published a message in sqs

---
+++ 3.Sqs/src/SqsPublisher/Contracts/CustomerCreated.cs
@@ -0,0 +1,18 @@
+namespace SqsPublisher.Contracts;
+
+public record CustomerCreated
+{
+    public required Guid Id { get; init; }
+    public required Name FullName { get; init; }
+    public required string Email { get; init; }
+    public required string GithubUsername { get; init; }
+    public required DateTime DateOfBirth { get; init; }
+
+}
+
+public record Name
+{
+    public required string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public required string LastName { get; set; }
+}
---
+++ 3.Sqs/src/SqsPublisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher.Contracts;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "ShadowTheHedgehog@gmail.com",
+    FullName = new Name
+    {
+        FirstName = "Shadow",
+        LastName = "Hedgehog"
+    },
+    DateOfBirth = new DateTime(2000, 01, 01),
+    GithubUsername = "IAmFasterThanSonic"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer)
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
