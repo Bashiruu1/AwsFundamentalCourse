9254dc9bb94d5e769933aaa727c09adfda94039a
Usman <bashiruu1@gmail.com>
2023-12-12 20:50:01 -0500

    s3 bucket versioning

--- 6.S3/terraform/modules/s3/main.tf
+++ 6.S3/terraform/modules/s3/main.tf
@@ -2,3 +2,10 @@ resource "aws_s3_bucket" "this" {
     bucket = var.bucket_name
     
 }
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
